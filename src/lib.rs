c9de6a03817957b8a26c8cf3c8c925485734b0d3
gifnksm <makoto.nksm+github@gmail.com>
2017-05-29 22:29:30 +0900

    Add warning lints

--- src/lib.rs
+++ src/lib.rs
@@ -7,9 +7,17 @@
 
 //! Performs topological sorting.
 
-#![warn(bad_style, missing_docs,
-        unused, unused_extern_crates, unused_import_braces,
-        unused_qualifications, unused_results)]
+#![warn(bad_style)]
+#![warn(missing_copy_implementations)]
+#![warn(missing_debug_implementations)]
+#![warn(missing_docs)]
+#![warn(trivial_casts)]
+#![warn(trivial_numeric_casts)]
+#![warn(unused)]
+#![warn(unused_extern_crates)]
+#![warn(unused_import_braces)]
+#![warn(unused_qualifications)]
+#![warn(unused_results)]
 
 use std::cmp::Ordering;
 use std::collections::{HashMap, HashSet};
